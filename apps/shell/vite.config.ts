c3ed4b182b88912ad0133506252e32f03768c391
amallen <alex.mallengines@gmail.com>
2025-06-19 09:44:06 +0200

    fix to use app at production

--- apps/shell/vite.config.ts
+++ apps/shell/vite.config.ts
@@ -2,16 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
+console.log('isProduction ==> ', isProduction)
+// Reemplaza esta URL con la URL base de tu despliegue en S3/CloudFront
+const productionRemoteBaseUrl = 'https://stage.resumecoach.com'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     federation({
       name: 'shell',
-      remotes: {
-        product: 'http://localhost:5001/assets/remoteEntry.js',
-        ui: 'http://localhost:5002/assets/remoteEntry.js'
-      },
+      remotes: isProduction
+      ? {
+          product: `${productionRemoteBaseUrl}/product/assets/remoteEntry.js`,
+          ui: `${productionRemoteBaseUrl}/ui/assets/remoteEntry.js`
+        }
+      : {
+          product: 'http://localhost:5001/assets/remoteEntry.js',
+          ui: 'http://localhost:5002/assets/remoteEntry.js'
+        },
       shared: ['react', 'react-dom']
     })
   ],
@@ -19,7 +30,7 @@ export default defineConfig({
     modulePreload: false,
     target: 'esnext',
     minify: false,
-    cssCodeSplit: false
+    cssCodeSplit: false,
   },
   server: {
     port: 5000
